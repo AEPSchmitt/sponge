94140e052a7a021acc636d3b28c10774b5b166dc
Adam <s134873@student.dtu.dk>
2017-01-04 15:48:04 +0100

    simple xml parse

--- NodejsConsoleApp1/NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/NodejsConsoleApp1/app.js
@@ -1,8 +1,15 @@
 "use strict";
 const fs = require('fs');
+const parseString = require('xml2js').parseString;
 
-fs.readdir(__dirname, (err, files) => {
-    files.forEach(file => {
-    console.log(file);
+fs.readdir(__dirname, function(err, files){
+    files.forEach(function (file){
+        if (file.indexOf('.xml') != -1) {
+            parseString(xml, function (err, result) {
+                console.dir(result);
+            });
+        } else {
+            console.log(file);
+        }
     });
 })
